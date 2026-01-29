85f35cb5e14051cb779b7b2a5cf21282ad654136
zablon <zablonoigo51@gmail.com>
2026-01-29 16:57:55 +0300

    add project config

---
+++ src/config.py
@@ -0,0 +1,31 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+class Settings(BaseSettings):
+    DATABASE_URL: str
+    JWT_SECRET:str
+    JWT_ALGORITHM:str
+    REDIS_HOST: str
+    REDIS_PORT: int
+    MAIL_USERNAME: str
+    MAIL_PASSWORD: str
+    MAIL_FROM: str
+    MAIL_PORT: int
+    MAIL_SERVER: str
+    MAIL_FROM_NAME: str
+    MAIL_STARTTLS: bool = True
+    MAIL_SSL_TLS: bool = False
+    USE_CREDENTIALS: bool = True
+    VALIDATE_CERTS: bool = True
+    APP_NAME: str
+    DOMAIN: str
+    CELERY_RESULT_BACKEND: str
+    CELERY_BROKER_URL: str
+
+    model_config = SettingsConfigDict(
+        env_file=".env",
+        extra="ignore"
+    )
+
+Config = Settings()
+
+broker_connection_retry_on_startup = True
