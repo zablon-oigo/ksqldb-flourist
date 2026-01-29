ead5ec93491f56ed8f38f68c2bc38b9dc70ebe61
zablon <zablonoigo51@gmail.com>
2026-01-29 22:06:52 +0300

    add users model

--- src/db/models.py
+++ src/db/models.py
@@ -2,8 +2,37 @@ from sqlmodel import SQLModel, Field, Relationship, Column
 from typing import Optional, List 
 from sqlalchemy import String
 from enum import Enum as pyEnum 
-from uuid import UUID, uuid4 
-from datetime import datetime 
+import uuid
+from datetime import datetime, timezone
+import sqlalchemy.dialects.mysql as mysql
+from sqlalchemy import func, ForeignKey
+
+
+class User(SQLModel, table=True):
+    __tablename__ = "users"
+
+    uid: str = Field(
+        sa_column=Column(
+            mysql.CHAR(36),
+            primary_key=True,
+            unique=True,
+            nullable=False,
+            default=lambda: str(uuid.uuid4())
+        )
+    )
+    username: str = Field(sa_column=Column(mysql.VARCHAR(50), nullable=False))
+    email: str = Field(sa_column=Column(mysql.VARCHAR(100), nullable=False, unique=True, index=True))
+    first_name: Optional[str] = Field(sa_column=Column(mysql.VARCHAR(50), nullable=True))
+    last_name: Optional[str] = Field(sa_column=Column(mysql.VARCHAR(50), nullable=True))
+    is_admin: bool = False
+    is_verified: bool = Field(sa_column=Column(mysql.BOOLEAN, default=False))
+    password_hash: str = Field(sa_column=Column(mysql.VARCHAR(255), nullable=False))
+    created_at: datetime = Field(sa_column=Column(mysql.DATETIME, nullable=False, default=datetime.now(timezone.utc)))
+    updated_at: datetime = Field(sa_column=Column(mysql.DATETIME, nullable=False, default=datetime.now(timezone.utc), onupdate=datetime.now(timezone.utc)))
+
+    def __repr__(self):
+        return f"<User {self.username}>"
+    
 
 
 
@@ -11,4 +40,5 @@ class Frequency(str, pyEnum):
     daily="daily"
     weekly="weekly"
     bi_weekly= "bi-weekly"
-    monthly= "monthly"
+    monthly= "monthly" 
+
