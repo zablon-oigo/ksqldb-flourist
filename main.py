74887571bdf141d98c8f8d6d375736fd940ca40c
zablon <zablonoigo51@gmail.com>
2026-01-31 20:35:34 +0300

    initialize database

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
-from fastapi import FastAPI 
+from fastapi import FastAPI, Header, status, Request
+from typing import Optional
+from contextlib import asynccontextmanager
 
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    """Initialize database"""
+    print("Starting server... initializing database")
+    await initdb()
+    yield
+    print("Server is stopping...")
 app= FastAPI()
 
 
