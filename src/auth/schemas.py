931bcd1906453d3eb79e1bed0b90b071b9ce6ef1
zablon <zablonoigo51@gmail.com>
2026-02-01 07:48:52 +0300

    add user create schema

---
+++ src/auth/schemas.py
@@ -0,0 +1,27 @@
+from pydantic import BaseModel, EmailStr, Field
+from typing import List, Optional
+from datetime import datetime
+import uuid
+
+
+class UserCreateModel(BaseModel):
+    email: EmailStr
+    username: str
+    first_name: str
+    last_name: str
+    password: str
+    role: Optional[str] = "user"
+
+    model_config = {
+        "json_schema_extra": {
+            "example": {
+                "first_name": "John",
+                "last_name": "Doe",
+                "username": "johndoe",
+                "email": "johndoe123@co.com",
+                "password": "testpass123",
+                "role": "user"
+            }
+        }
+    }
+
