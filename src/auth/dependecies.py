f3070cf41bd9d5292dee0cfed8c58e5ee558dc6d
zablon <zablonoigo51@gmail.com>
2026-02-13 16:24:02 +0300

    role checker

--- src/auth/dependecies.py
+++ src/auth/dependecies.py
@@ -75,3 +75,15 @@ async def get_current_user(
     return user
 
 
+class RoleChecker:
+    def __init__(self, allowed_roles: List[str]) -> None:
+        self.allowed_roles = allowed_roles
+
+    def __call__(self, current_user: User = Depends(get_current_user)) -> bool:
+        if not current_user.is_verified:
+            raise AccountNotVerified()
+        if current_user.role in self.allowed_roles:
+            return True
+
+        raise InsufficientPermission()
+    
