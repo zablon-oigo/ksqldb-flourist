cf1763c22979db13e23f96e542cffe4f62f4f3fa
zablon <zablonoigo51@gmail.com>
2026-02-14 19:44:10 +0300

    mail config

---
+++ src/mail.py
@@ -0,0 +1,21 @@
+from pathlib import Path
+from fastapi_mail import FastMail, ConnectionConfig, MessageSchema, MessageType
+from src.config import Config
+
+BASE_DIR = Path(__file__).resolve().parent
+
+mail_config = ConnectionConfig(
+    MAIL_USERNAME=Config.MAIL_USERNAME,
+    MAIL_PASSWORD=Config.MAIL_PASSWORD,  
+    MAIL_FROM=Config.MAIL_FROM,
+    MAIL_PORT=587,
+    MAIL_SERVER="smtp.gmail.com",
+    MAIL_STARTTLS=True,      
+    MAIL_SSL_TLS=False,      
+    USE_CREDENTIALS=True,
+    VALIDATE_CERTS=True,     
+)
+
+mail = FastMail(mail_config)
+
+
